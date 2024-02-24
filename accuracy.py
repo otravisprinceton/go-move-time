dd376dac0884cab12153165e3650b04a33e68e46
Owen Travis <otravis710@gmail.com>
2024-02-24 13:08:55 -0500

    Manual test

--- accuracy.py
+++ accuracy.py
@@ -244,7 +244,10 @@ def main_helper(filepath, data_folder, dfs):
     # output_filepath = os.path.normpath(os.path.join(data_folder, "../localtmpout/", filepath[7:-4] + "-out.txt"))
 
     # DELLA
-    output_filepath = data_folder[:-7] + "OutputAccuracyFeb13-24/" + filepath[:-4] + "-acc.txt"
+    # output_filepath = data_folder[:-7] + "OutputAccuracyFeb13-24/" + filepath[:-4] + "-acc.txt"
+
+    # Testing
+    output_filepath = "/scratch/gpfs/otravis/tmp_manual_test_out.txt"
 
     print("Saving to " + str(output_filepath))
     print("Running katago.")
@@ -267,7 +270,7 @@ def main_helper(filepath, data_folder, dfs):
 def main():
     dfs = []
 
-    is_array_job = True
+    is_array_job = False
     on_cluster = True
 
     if is_array_job:
@@ -283,15 +286,19 @@ def main():
     with open(os.path.join(data_folder, "gamesList.txt"), "r") as gamesList:
         filenames = gamesList.readlines()
 
-    for i in range(len(filenames)):
-        if i % NUMJOBS == job_idx or job_idx == -1:
-            main_helper(filenames[i].strip(), data_folder, dfs)
+    # for i in range(len(filenames)):
+    #     if i % NUMJOBS == job_idx or job_idx == -1:
+    #         main_helper(filenames[i].strip(), data_folder, dfs)
+        
+    main_helper(filenames[0].strip(), data_folder, dfs)
 
     # main_helper("test_weird_passes.sgf", data_folder, dfs)
 
     # Della
-    pd.concat(dfs, ignore_index=True).to_csv(f'/scratch/gpfs/otravis/accuracy-{job_idx}.csv')
-    # pd.concat(dfs, ignore_index=True).to_csv(f'/scratch/gpfs/otravis/tmp-testing.csv')
+    # pd.concat(dfs, ignore_index=True).to_csv(f'/scratch/gpfs/otravis/tmp_accuracy-{job_idx}.csv')
+
+    # Test
+    pd.concat(dfs, ignore_index=True).to_csv('/scratch/gpfs/otravis/tmp_manual_test_out.csv')
 
     # Local
     # pd.concat(dfs, ignore_index=True).to_csv(f'/Users/owentravis/Documents/IW/go-move-time/tmp-out.csv')
